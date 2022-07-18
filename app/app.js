58471f9fcfa7c626adc3ecb65977f13bc23d7855
Lasse Lang <lang.lasse@gmx.de>
2022-07-18 20:18:05 +0200

    Added multiline text with textarea animation and fixed sending emthy messages

--- app/app.js
+++ app/app.js
@@ -14,8 +14,28 @@ socket.on("message", message => {
   messages.appendChild(el);
 });
 
+textInput.addEventListener("input", () => {
+  if (textInput.value.split("\n").length > 1) {
+    textInput.style.height = "150px";
+    textInput.style.borderTopLeftRadius = "15px";
+    textInput.style.borderTopRightRadius = "15px";
+  }
+  else {
+    textInput.style.height = "30px";
+    textInput.style.borderTopLeftRadius = "0px";
+    textInput.style.borderTopRightRadius = "0px";
+  }
+});
+
 document.getElementById("btn-send").addEventListener("click",  () => {
-  text = textInput.value.replace(/</g, "&lt;");
-  text = text.replace(/>/g, "&gt;");
-  socket.emit("message", {text: text, user: user});
+  if (textInput.value.trim().length != 0) {
+    text = textInput.value.replace(/</g, "&lt;");
+    text = text.replace(/>/g, "&gt;");
+    text = text.replace(/\n/g, "<br>")
+    socket.emit("message", {text: text, user: user});
+    textInput.value = "";
+    textInput.style.height = "30px";
+    textInput.style.borderTopLeftRadius = "0px";
+    textInput.style.borderTopRightRadius = "0px";
+  }
 });
